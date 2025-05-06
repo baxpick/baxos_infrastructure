4e6553814ae8b7781cf5966106d20731476cb5b6
Bax Picker <baxpicker@gmail.com>
2025-05-06 14:42:56 +0000

    Fix environment variable visibility in dev container

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,21 +1,32 @@
 ENVIRONMENT=${1:-"dev"}
 FILE_secrets=".devcontainer/.env.${ENVIRONMENT}"
 
-if [[ -n "${ARM_CLIENT_ID}" && -n "${ARM_CLIENT_SECRET}" && -n "${ARM_TENANT_ID}" && -n "${ARM_SUBSCRIPTION_ID}" ]]; then
+if  [ -n "${ARM_CLIENT_ID}" ] && \
+    [ -n "${ARM_CLIENT_SECRET}" ] && \
+    [ -n "${ARM_TENANT_ID}" ] && \
+    [ -n "${ARM_SUBSCRIPTION_ID}" ]; then
+
     echo "Using pre-configured environment variables"
-elif [[ -f "${FILE_secrets}" ]]; then
-    echo "Loading secret environment variables from '${FILE_secrets}'"
-    set -a
-    source "${FILE_secrets}"
-    set +a
+
+elif [ -f "${FILE_secrets}" ]; then
+
+    echo "Loading secrets from ${FILE_secrets}..."
+
+    cp "${FILE_secrets}" ~/.devcontainer_env
+    grep -q "devcontainer_env" ~/.bashrc || echo 'if [ -f ~/.devcontainer_env ]; then source ~/.devcontainer_env; fi' >> ~/.bashrc
+    . ~/.devcontainer_env
 else
-    echo "No secrets file found for environment '${ENVIRONMENT}' "
-    echo "Please set variables from .env.example file"
+    echo "No secrets file found for environment: ${ENVIRONMENT}"
+    
     exit 1
 fi
 
-if [[ -n "$ARM_CLIENT_ID" && -n "$ARM_CLIENT_SECRET" && -n "$ARM_TENANT_ID" ]]; then
-    az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+if  [ -n "${ARM_CLIENT_ID}" ] && \
+    [ -n "${ARM_CLIENT_SECRET}" ] && \
+    [ -n "${ARM_TENANT_ID}" ] && \
+    [ -n "${ARM_SUBSCRIPTION_ID}" ]; then
+    
+    az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
 else
     echo "Missing required Azure credentials. Azure login skipped."
 fi
