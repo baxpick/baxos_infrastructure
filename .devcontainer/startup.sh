8945feaa5b3e74e6bfb2854d8fa77ee04e8cf27e
Bax Picker <baxpicker@gmail.com>
2025-05-15 12:23:18 +0000

    Add aws cli

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,10 +1,17 @@
 ENVIRONMENT=${1:-"dev"}
 FILE_secrets=".devcontainer/.env.${ENVIRONMENT}"
 
+# Load secrets
+# ############
+
 if  [ -n "${ARM_CLIENT_ID}" ] && \
     [ -n "${ARM_CLIENT_SECRET}" ] && \
     [ -n "${ARM_TENANT_ID}" ] && \
-    [ -n "${ARM_SUBSCRIPTION_ID}" ]; then
+    [ -n "${ARM_SUBSCRIPTION_ID}" ] && \
+    \
+    [ -n "${AWS_ACCESS_KEY_ID}" ] && \
+    [ -n "${AWS_SECRET_ACCESS_KEY}" ] && \
+    [ -n "${AWS_DEFAULT_REGION}" ]; then 
 
     echo "Using pre-configured environment variables"
 
@@ -22,12 +29,47 @@ else
     exit 1
 fi
 
+# AZURE LOGIN
+# ###########
+
 if  [ -n "${ARM_CLIENT_ID}" ] && \
     [ -n "${ARM_CLIENT_SECRET}" ] && \
     [ -n "${ARM_TENANT_ID}" ] && \
     [ -n "${ARM_SUBSCRIPTION_ID}" ]; then
     
-    az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
+    if command -v az >/dev/null; then
+        if az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}; then
+            echo "[AZURE LOGIN] Azure CLI login successful."
+        else
+            echo "[AZURE LOGIN] Azure CLI login failed."
+        fi
+    else
+        echo "[AZURE LOGIN] Azure CLI not found."
+    fi
 else
-    echo "Missing required Azure credentials. Azure login skipped."
+    echo "[AZURE LOGIN] Missing required credentials. Login skipped."
+fi
+
+# AWS LOGIN
+# #########
+
+if  [ -n "${AWS_ACCESS_KEY_ID}" ] && \
+    [ -n "${AWS_SECRET_ACCESS_KEY}" ] && \
+    [ -n "${AWS_DEFAULT_REGION}" ]; then
+    
+    if command -v aws >/dev/null; then
+        if \
+            aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && \
+            aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY && \
+            aws configure set default.region $AWS_DEFAULT_REGION; then
+            
+            echo "[AWS LOGIN] AWS CLI login successful."
+        else
+            echo "[AWS LOGIN] AWS CLI login failed."
+        fi
+    else
+        echo "[AWS LOGIN] AWS CLI not found."
+    fi
+else
+    echo "[AWS LOGIN] Missing required credentials. Login skipped."
 fi
